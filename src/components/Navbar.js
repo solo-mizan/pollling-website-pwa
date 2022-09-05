987dcc2d530f6d4d5c33f9db20017475229fae01
solo-mizan <mizanbd.eee@gmail.com>
2022-09-06 00:06:53 +0600

    multiple votes stopped using localstorage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
@@ -6,18 +7,17 @@ const Navbar = () => {
             <div className="navbar bg-neutral text-neutral-content">
                 <div className="navbar-start">
                     <div className="dropdown">
-                        <label tabindex={0} className="btn btn-ghost btn-circle">
+                        <label tabIndex={0} className="btn btn-ghost btn-circle">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                         </label>
-                        <ul tabindex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Homepage</a></li>
-                            <li><a>Portfolio</a></li>
-                            <li><a>About</a></li>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-neutral text-neutral-content rounded-box w-52">
+                            <li><Link to={'/home'}>Homepage</Link></li>
+                            <li><a href='https://mizanbd.netlify.app' target='_blank' rel='noreferrer'>Portfolio</a></li>
                         </ul>
                     </div>
                 </div>
                 <div className="navbar-center">
-                    <a className="btn btn-ghost normal-case text-xl">Vote for India!</a>
+                    <Link to='/vote' className="btn btn-ghost normal-case text-xl">Vote for India!</Link>
                 </div>
                 <div className="navbar-end">
                     <button className="btn btn-ghost btn-circle">
--- src/components/Vote.js
+++ src/components/Vote.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 import voteBG from '../assets/voteBG.png';
 
 const Vote = () => {
+    const [option, setOption] = useState('');
+
+    const handleChange = (e) => {
+        setOption(e.target.value);
+    }
+
+    useEffect(() => {
+    }, [option, setOption])
+
+    const handleConfirmVote = () => {
+        if (localStorage.getItem('isVoted')) {
+            toast.warning('You have already voted!')
+        }
+        else {
+            localStorage.setItem('isVoted', option)
+            toast.success('Your vote has been received!')
+        }
+    }
+
     return (
-        <div>
-            <select className="select select-success w-full max-w-sm text-center m-5">
-                <option disabled selected>Pick your favorite political party</option>
-                <option>BJP Bharatiya Janata Party</option>
-                <option>Indian National Congress</option>
-                <option>BSP Bahujan Samaj Party</option>
-                <option>CPI Communist Party of India</option>
-                <option>CPM Communist Party of India (Marxist)</option>
-            </select>
-            <button className='btn btn-outline'>Confirm your vote</button>
+        <div className='items-center justify-center text-center mx-auto'>
+            <div>
+                <select className="select select-success w-full max-w-sm text-center m-5" onChange={handleChange}>
+                    <option disabled selected>Pick your favorite political party</option>
+                    <option>BJP Bharatiya Janata Party</option>
+                    <option>Indian National Congress</option>
+                    <option>BSP Bahujan Samaj Party</option>
+                    <option>CPI Communist Party of India</option>
+                    <option>CPM Communist Party of India (Marxist)</option>
+                </select>
+                {
+                    localStorage.getItem('isVoted') ? <h3 className='text-xl font-mono text-center m-5 text-success'> You voted for {localStorage.getItem('isVoted')} </h3> :
+                        <h3 className='text-xl font-mono text-center m-5 text-error'>Your vote didn't cast yet...</h3>
+                }
+                <button className='btn btn-outline' onClick={handleConfirmVote}>Confirm your vote</button>
+            </div>
         </div>
     );
 };
